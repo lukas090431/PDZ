f108a23d1e7b13fb660dd92ff6929d81da5d82f8
lukas090431 <lukas09043@gmail.com>
2016-04-28 20:58:22 +0100

    git_1

--- logowanie.cpp
+++ logowanie.cpp
@@ -10,21 +10,49 @@ int login(){
 	cin.ignore();
 	cout<<"Podaj haslo "<<endl;
 	cin>>b;
-	cin.ignore();
 	
-	if(a=="root" && b=="toor")
+	cin.ignore();
+	if(a=="ti" && b=="toor")
 	{
-		cout<<"Witam w lesie Walusioka"<<endl;
-		
+		cout<<"Witaj techniku informatyki"<<endl;
+		return 1;
+	}
+	else
+	{
+		if(a=="te" && b=="toor")
+	{
+			cout<<"Witaj techniku elektroniki"<<endl;
+			return 2;
+	}
+	else
+	{
+			if(a=="teo" && b=="toor")
+	{
+				cout<<"Witaj techniku energetyki odnawialnej"<<endl;
+				return 3;
+	}
+	else
+	{
+				if(a=="tm" && b=="toor")
+	{
+					cout<<"Witaj techniku mechatroniki"<<endl;
+					return 4;
 	}
 	else
 	{
 		cout<<"Bledne haslo lub login"<<endl;
-		
+		return 0;
+	}
+	}
+	}
 	}
 }
 
 int main(){
-	login();
+	int wynik_logowania=login();
+	if( wynik_logowania == 1 )
+	{
+		cout<<"Rozpoczeto modul informatyczny"<<endl;
+	}
 	getch();
 }
